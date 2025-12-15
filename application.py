a14b4dfed79ccca4debc918ffac5f2935f464b28
Saravanansubramaniyan123 <146221808+Saravanansubramaniyan123@users.noreply.github.com>
2025-12-15 12:59:07 +0530

    original

---
+++ application.py
@@ -0,0 +1,31 @@
+from flask import Flask,request,render_template,jsonify
+import pandas as pd
+import numpy as np
+import pickle
+
+std = pickle.load(open("models/StandardScaler.pkl","rb"))
+model = pickle.load(open("models/lin_model.pkl","rb"))
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('home.html')
+
+@app.route("/prediction",methods = ["GET","POST"])
+def prediction():
+    if request.method == "POST":
+        features = [float(request.form["Temperature"]),float(request.form["RH"]),float(request.form['Ws']),float(request.form["Rain"]),
+                    float(request.form["FFMC"]),float(request.form["DMC"]),float(request.form['ISI']),
+                    float(request.form["Classes"]),float(request.form["Region"])]
+       
+        scaled = std.transform([features])#for 2d conversion i used square bracket
+        prediction = model.predict(scaled)[0]
+        return render_template("prediction.html",prediction = prediction)
+    else:
+        return render_template("prediction.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
